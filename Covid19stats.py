940469f76ce4fdb17306e77d9144a7a57f37f5e8
sdutz <zambellilorenzo@gmail.com>
2020-11-12 18:23:01 +0100

    Create CovStats class

--- Covid19stats.py
+++ Covid19stats.py
@@ -26,15 +26,93 @@ def is_connected():
         return False
 
 #----------------------------------------------------------------
-class CowWnd(wx.Frame): 
+class CovStats(): 
+    '''Stats class'''
+    def __init__(self, size):
+        self.baseUrl = 'https://statistichecoronavirus.it'
+        self.days = ["Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato","Domenica"]
+        base = os.path.realpath(__file__)[:-3]
+        self.iniFile = base + '.ini'
+        self.pic = base + '.png'
+        self.respic = base + 'res.png'
+        self.size = size
+        
+#----------------------------------------------------------------
+    def getValues(self, region, city):
+        '''retrieve values from website'''
+        if not is_connected():
+            return False, 'nessuna connessione di rete presente'
+        try:
+            page = requests.get(self.baseUrl + '/coronavirus-italia/coronavirus-' + region + '/coronavirus-' + city +'/')
+        except requests.exceptions.RequestException as e:
+            return False, 'impossibile stabilire la connessione con la fonte\n' + str(e)
+        allData = re.findall(r'data:.*', page.text, re.MULTILINE)
+        if len(allData) < 4:
+            return False, 'dati non disponibili per: '+ region + ' ' + city
+        res = allData[3]
+        values = [int(x) for x in res[res.find('[') + 1 : res.find(']') - 1].split(',')]
+        self.getGraph(values)
+        return True, self.getStats(values)
+
+#----------------------------------------------------------------
+    def getGraph(self, values):
+        '''plot graph'''
+        pyplot.plot(numpy.diff(values))
+        pyplot.ylabel('')
+        pyplot.xlabel('')
+        pyplot.savefig(self.pic)
+        pyplot.close()
+        with open(self.pic, 'r+b') as file, Image.open(file) as image:
+            resizeimage.resize_cover(image, self.size).save(self.respic, image.format)
+
+#----------------------------------------------------------------
+    def getStats(self, values):
+        '''performs stats'''
+        today = datetime.date.today()
+        res = self.days[today.weekday()] + ' ' + str(today.strftime('%d/%m/%Y')) + '\n'
+        diff = values[-1] - values[-2]
+        diff = str(diff) if diff < 0 else '+' + str(diff)
+        res += str(values[-1]) + ' ultimi nuovi positivi ('  + diff + ')\n'
+        res += 'statistiche sugli ultimi ' + str(len(values)) + ' giorni:' + '\n'
+        res += 'media giornaliera: ' + str(round(statistics.mean(values))) + '\n'
+        res += 'totale nuovi positivi: ' + str(numpy.sum(values)) + '\n'
+        res += 'minimo: ' + str(min(values)) + ', massimo: ' + str(max(values))
+        return res
+
+#----------------------------------------------------------------
+    def loadConfig(self):
+        '''Load configuration from ini file'''
+        config = configparser.ConfigParser()
+        config.read(self.iniFile)
+        if 'General' in config.sections():
+            region = config["General"]["Region"]
+            city   = config["General"]["City"]
+            return region, city
+        else:
+            return 'Lombardia', 'Bergamo'
+
+#----------------------------------------------------------------
+    def saveConfig(self, region, city):
+        '''Save configuration to ini file'''
+        config = configparser.ConfigParser()
+        config["General"] = {}
+        config["General"]["Region"] = region
+        config["General"]["City"] = city
+        with open(self.iniFile, 'w') as configFile:
+            config.write(configFile)
+    
+
+#----------------------------------------------------------------
+class CovWnd(wx.Frame): 
     '''Main Window class'''
-    def __init__(self, parent, title): 
+    def __init__(self, parent, title):
         '''Constructor'''
         self.size = (250, 420)
-        super(CowWnd, self).__init__(parent, title = title, size = self.size, style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
-        self.initData()
-        self.loadConfig()
-        self.initUI()
+        super(CovWnd, self).__init__(parent, title = title, size = self.size, style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
+        self.initItaly()
+        self.stats = CovStats((self.size[0] - 50, 150))
+        region, city = self.stats.loadConfig()
+        self.initUI(region, city)
         self.timer = None
         self.last = None
         self.showData()
@@ -42,21 +120,7 @@ class CowWnd(wx.Frame):
         self.Show()
 
 #----------------------------------------------------------------
-    def initData(self):
-        '''Init of all data'''
-        self.italy = {}
-        self.initItaly()
-        self.baseUrl = 'https://statistichecoronavirus.it'
-        self.days = ["Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato","Domenica"]
-        self.region = 'Lombardia'
-        self.city = 'Bergamo'
-        base = os.path.realpath(__file__)[:-3]
-        self.iniFile = base + '.ini'
-        self.pic = base + '.png'
-        self.respic = base + 'res.png'
-
-#----------------------------------------------------------------
-    def initUI(self):
+    def initUI(self, region, city):
         '''Init of user interface'''
         self.panel = wx.Panel(self) 
         box = wx.GridBagSizer()
@@ -64,21 +128,21 @@ class CowWnd(wx.Frame):
         static = wx.StaticText(self.panel, label = 'Regione', style = wx.LEFT) 
         box.Add(static, pos = (0, 0), flag = wx.EXPAND|wx.ALL, border = 5)
         self.regions = wx.Choice(self.panel, choices = regions)
-        self.regions.SetSelection(regions.index(self.region))
+        self.regions.SetSelection(regions.index(region))
         box.Add(self.regions, pos = (0, 1), flag = wx.EXPAND|wx.ALL, border = 5) 
 
         static = wx.StaticText(self.panel, label = 'Provincia', style = wx.ALIGN_LEFT)   
         box.Add(static, pos = (1, 0), flag = wx.EXPAND|wx.ALL, border = 5)
-        cities = list(self.italy[self.region])
+        cities = list(self.italy[region])
         self.cities = wx.Choice(self.panel, choices = cities)
-        self.cities.SetSelection(cities.index(self.city))
+        self.cities.SetSelection(cities.index(city))
         box.Add(self.cities, pos = (1, 1), flag = wx.EXPAND|wx.ALL, border = 5)
 
         self.result = wx.StaticText(self.panel, style = wx.ALIGN_CENTER)
         box.Add(self.result, pos = (2, 0), flag = wx.EXPAND|wx.ALL, border = 5, span = (1, 2))
         self.graph = wx.StaticBitmap(self.panel, style = wx.ALIGN_CENTER)
         box.Add(self.graph, pos = (3, 0), flag = wx.EXPAND|wx.ALL, border = 5, span = (2, 2))
-        about = wx.StaticText(self.panel, style = wx.ALIGN_CENTER, label = 'fonte: ' + self.baseUrl + '\n\n' + 'Made by sdutz')
+        about = wx.StaticText(self.panel, style = wx.ALIGN_CENTER, label = 'fonte: ' + self.stats.baseUrl + '\n\n' + 'Made by sdutz')
         box.Add(about, pos = (5, 0), flag = wx.EXPAND|wx.ALL, border = 5, span = (1, 2))
 
         self.regions.Bind(wx.EVT_CHOICE, self.OnRegions) 
@@ -90,7 +154,9 @@ class CowWnd(wx.Frame):
 #----------------------------------------------------------------
     def onClose(self, event):
         '''on Close event'''
-        self.saveConfig()
+        region = self.regions.GetString( self.regions.GetSelection())
+        city = self.cities.GetString( self.cities.GetSelection())
+        self.stats.saveConfig(region, city)
         if self.timer:
             self.timer.cancel()
         self.Destroy()
@@ -105,9 +171,9 @@ class CowWnd(wx.Frame):
     def onSearch(self):
         '''search dialogs'''
         dlg = wx.TextEntryDialog(self, 'Cerca la città')
-        if ( dlg.ShowModal() == wx.ID_OK):
+        if (dlg.ShowModal() == wx.ID_OK):
             if not self.doSearch(dlg.GetValue()):
-                wx.MessageBox('Nessun risultato trovato', parent=self)
+                wx.MessageBox('Nessun risultato trovato!', parent=self)
         dlg.Destroy()
 
 #----------------------------------------------------------------
@@ -129,6 +195,7 @@ class CowWnd(wx.Frame):
 #----------------------------------------------------------------
     def initItaly(self):
         '''init of all regions and cities of Italy'''
+        self.italy = {}
         self.italy["Abruzzo"]=sorted(["L'Aquila","Chieti","Pescara","Teramo"])
         self.italy["Basilicata"]=sorted(["Potenza","Matera"])
         self.italy["Calabria"]=sorted(["Reggio Calabria","Catanzaro","Crotone","Vibo Valentia Marina","Cosenza"])
@@ -151,88 +218,23 @@ class CowWnd(wx.Frame):
         self.italy["Veneto"]=sorted(["Venezia","Belluno","Padova","Rovigo","Treviso","Verona","Vicenza"])
 
 #----------------------------------------------------------------
-    def loadConfig(self):
-        '''Load configuration from ini file'''
-        config = configparser.ConfigParser()
-        config.read(self.iniFile)
-        if 'General' in config.sections():
-            self.region = config["General"]["Region"]
-            self.city   = config["General"]["City"]
-
-#----------------------------------------------------------------
-    def saveConfig(self):
-        '''Save configuration to ini file'''
-        config = configparser.ConfigParser()
-        config["General"] = {}
-        config["General"]["Region"] = self.regions.GetString( self.regions.GetSelection())
-        config["General"]["City"] = self.cities.GetString( self.cities.GetSelection())
-        with open(self.iniFile, 'w') as configFile:
-            config.write(configFile)
-
-#----------------------------------------------------------------
     def OnRegions(self, event):
         self.cities.SetItems(self.italy[self.regions.GetString(self.regions.GetSelection())])
         self.cities.SetSelection(0)
         self.showData()
 
 #----------------------------------------------------------------
-    def getValues(self):
-        '''retrieve values from website'''
-        if not is_connected():
-            self.result.SetLabel('nessuna connessione di rete presente')
-            self.startTimer(False)
-            return
-        region = self.cleanName(self.regions.GetString(self.regions.GetSelection()))
-        city = self.cleanName(self.cities.GetString(self.cities.GetSelection()))
-        try:
-            page = requests.get(self.baseUrl + '/coronavirus-italia/coronavirus-' + region + '/coronavirus-' + city +'/')
-        except requests.exceptions.RequestException as e:
-            self.result.SetLabel('impossibile stabilire la connessione con la fonte\n' + str(e))
-            self.startTimer(False)
-            return
-        allData = re.findall(r'data:.*', page.text, re.MULTILINE)
-        if len(allData) < 4:
-            self.result.SetLabel('dati non disponibili per: ' + region + ' ' + city)
-            self.startTimer(False)
-            return
-        res = allData[3]
-        return [int(x) for x in res[res.find('[') + 1 : res.find(']') - 1].split(',')]
-
-#----------------------------------------------------------------
-    def setGraph(self, values):
-        '''plot graph'''
-        pyplot.plot(numpy.diff(values))
-        pyplot.ylabel('')
-        pyplot.xlabel('')
-        pyplot.savefig(self.pic)
-        pyplot.close()
-        with open(self.pic, 'r+b') as file, Image.open(file) as image:
-            resizeimage.resize_cover(image, [self.size[0] - 50, 150]).save(self.respic, image.format)
-        self.graph.SetBitmap(wx.Bitmap(self.respic))
-
-#----------------------------------------------------------------
-    def setStats(self, values):
-        '''performs stats'''
-        today = datetime.date.today()
-        res = self.days[today.weekday()] + ' ' + str(today.strftime('%d/%m/%Y')) + '\n'
-        diff = values[-1] - values[-2]
-        diff = str(diff) if diff < 0 else '+' + str(diff)
-        res += str(values[-1]) + ' ultimi nuovi positivi ('  + diff + ')\n'
-        res += 'statistiche sugli ultimi ' + str(len(values)) + ' giorni:' + '\n'
-        res += 'media giornaliera: ' + str(round(statistics.mean(values))) + '\n'
-        res += 'totale nuovi positivi: ' + str(numpy.sum(values)) + '\n'
-        res += 'minimo: ' + str(min(values)) + ', massimo: ' + str(max(values))
-        self.result.SetLabel(res)
-
-#----------------------------------------------------------------
     def showData(self):
         '''show all data about selected city'''
         start = time.process_time()
-        values = self.getValues()
-        if not values:
+        region = self.cleanName(self.regions.GetString(self.regions.GetSelection()))
+        city = self.cleanName(self.cities.GetString(self.cities.GetSelection()))
+        ok, stat = self.stats.getValues(region, city)
+        self.result.SetLabel(stat)
+        if not ok:
+            self.startTimer(False)
             return
-        self.setGraph(values)
-        self.setStats(values)
+        self.graph.SetBitmap(wx.Bitmap(self.stats.respic))
         self.last = time.process_time()
         print('retrieved in ' + str(self.last - start) + ' s')
         self.panel.SetSize(self.size)
@@ -262,5 +264,5 @@ class CowWnd(wx.Frame):
 #----------------------------------------------------------------
 if __name__ == "__main__":              
     app = wx.App() 
-    CowWnd(None, 'Bilancio Covid') 
+    CovWnd(None, 'Bilancio Covid') 
     app.MainLoop()
